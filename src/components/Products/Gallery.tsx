2dd172b45a000de7a277912c69fb71e48300452b
B.Emilien <devwebjs10@gmail.com>
2024-03-30 11:24:15 +0100

    modification design du button des card ainsi que de l'affichage des catégories

--- src/components/Products/Gallery.tsx
+++ src/components/Products/Gallery.tsx
@@ -14,39 +14,41 @@ type productInfos = {
 export const Gallery: FunctionComponent = (): React.JSX.Element => {
 
   return (
-    <section className={clsx("w-[100%] max-w-screen h-auto flex justify-center items-center flex-wrap pt-20 bg-pink-100")}>   
-            {
+    <>
+      <section className={clsx("w-[100%] max-w-screen h-auto flex justify-center items-center flex-wrap pt-20 bg-pink-100")}>   
+              {
                 productInfos.map(product => (
                   <article 
-                    key={product.id}
-                    className={"m-10 p-1 bg-slate-50 rounded cursor-pointer hover:shadow-[0_35px_60px_0px_rgba(0,0,0)] transition ease-in-out hover:-translate-y-5 hover:scale-105 duration-700"}
-                  >
-                    <img
-                        className={"w-[350px] h-[250px] rounded"}        
-                        key={product.id}
-                        src={product.src}      
-                    />
-                    <div className={"flex justify-between items-center p-1"}>
-                      <h2 className={"font-tangerine text-4xl font-bold text-slate-900"}>
-                        {product.productName}
-                      </h2>
-                      <span className={"font-cormorantGaramond font-bold text-xl text-slate-900"}>
-                        Prix: {product.price}€
-                      </span>
-                    </div>
+                  key={product.id}
+                      className={"m-10 p-1 bg-slate-50 rounded hover:shadow-[0_35px_60px_0px_rgba(0,0,0)] transition ease-in-out hover:-translate-y-5 hover:scale-105 duration-700"}
+                    >
+                      <img
+                          className={"w-[350px] h-[250px] rounded"}        
+                          key={product.id}
+                          src={product.src}      
+                      />
+                      <div className={"flex justify-between items-center p-1"}>
+                        <h2 className={"font-tangerine text-4xl font-bold text-slate-900"}>
+                          {product.productName}
+                        </h2>
+                        <span className={"font-cormorantGaramond font-bold text-xl text-slate-900"}>
+                          Prix: {product.price}€
+                        </span>
+                      </div>
 
-                    <div className={"h-16 flex justify-between items-end p-1"}>
-                      <p className={clsx("flex justify-center items-center w-fit border-2 border-gray-300 bg-gray-300 rounded-2xl text-slate-950 text-base font-cormorantGaramond px-2")}>
-                        catégorie: {product.category}
-                      </p>
+                      <div className={"h-16 flex justify-between items-end p-1"}>
+                        <p className={clsx("flex justify-center items-center w-fit border-2 border-slate-700 bg-gray-300 rounded-2xl text-slate-950 text-base font-cormorantGaramond px-2 cursor-pointer hover:bg-slate-700 hover:text-neutral-50 hover:border-slate-700 transition-colors transition-duration-300")}>
+                          catégorie: {product.category}
+                        </p>
 
-                      <Button className={"block bg-slate-800 font-cormorantGaramond font-bold text-xl text-slate-50 px-2 py-1 rounded"}>
-                          Ajouter
-                      </Button>
-                    </div>
-                  </article>
-                ))
-            }
-    </section>
+                        <Button className={"block bg-slate-700 font-cormorantGaramond font-bold text-xl text-slate-50 px-2 rounded border-2 border-slate-700 hover:bg-gray-300 hover:text-slate-900 hover:border-slate-700"}>
+                            Voir
+                        </Button>
+                      </div>
+                    </article>
+                  ))
+              }
+      </section>
+    </>   
   )
 }
